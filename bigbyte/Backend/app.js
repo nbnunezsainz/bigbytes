78a80dc284de8fadf54a1460f4d630526a8cf04a
andresenriquez1 <andresem858@gmail.com>
2024-02-16 19:34:15 -0800

    added a few small changes

--- bigbyte/Backend/app.js
+++ bigbyte/Backend/app.js
@@ -6,21 +6,24 @@ const app = express();
 // Define your custom middleware first
 
 // Then, define the CORS middleware
+
+
+app.use((req, res, next) => {
+  console.log("Inside custom missleware");
+  res.status(200); // Set the status before sending any response
+  next(); // Call next to pass control to the next middleware/route handler
+}); 
+
+
 app.use(cors({
   origin: 'http://localhost:3000', // Replace with your frontend domain
   credentials: true, // Allow credentials (cookies, authentication headers, etc.)
 }));
 
 app.get('/', (req, res) => {
-  res.send('Hello from Express!'); // Send a simple response
+  res.status(200).send('Hello from Exp33resqqs!'); // Send a simple response
+  
 }); //when data sent ehre then it will follow to other middlewear
-
-app.use((req, res, next) => {
-  console.log("Inside custom middleware");
-  res.status(200); // Set the status before sending any response
-  next(); // Call next to pass control to the next middleware/route handler
-});
-
 
 
 module.exports = app;
