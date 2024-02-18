7ae7941ec087c324146975c0af71af7930b202e6
andresenriquez1 <andresem858@gmail.com>
2024-02-17 16:34:55 -0800

    small error in login.js fixed

--- bigbyte/Backend/controllers/Authentication.js
+++ bigbyte/Backend/controllers/Authentication.js
@@ -20,7 +20,7 @@ const admin = require('firebase-admin');
         });
 
         console.log('Successfully created new user:', userRecord.uid);
-        res.status(201).json({ message: 'User created successfully!' });
+        res.status(200).json({ message: 'User created successfully!' });
     } catch (error) {
         console.error('Error creating user:', error.message);
         res.status(500).json({ error: 'Error creating user' });
@@ -30,17 +30,19 @@ const admin = require('firebase-admin');
 exports.Login = async (req,res,next) =>
 {
     try {
+        console.log("hello");
         const { email, password } = req.body;
 
         // Create user using Firebase Admin SDK
-        const userCredential = await admin.auth().signInWithEmailAndPassword(email, password);
-        const token = await admin.auth().createCustomToken(userCredential.user.uid); 
+        //const userCredential = await admin.auth().signInWithEmailAndPassword(email, password);
+        //const token = await admin.auth().createCustomToken(userCredential.user.uid); 
         //need to handle token better
 
 
 
-        console.log('Successfully Login', userRecord.uid);
-        res.status(201).json({ message: 'User loginsuccessfully!' });
+        // console.log('Successfully Login', userRecord.uid);
+        console.log('Successfully Login');
+        res.status(200).json({ message: 'User loginsuccessfully!' });
     } catch (error) {
         console.error('Error logging user:', error.message);
         res.status(500).json({ error: 'Error loggin in for user' });
