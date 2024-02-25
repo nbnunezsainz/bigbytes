d47b185d4754a9204d6ccce23d2a865095bfa6c8
andresenriquez1 <andresem858@gmail.com>
2024-02-25 09:56:16 -0800

    auth added

--- bigbyte/Backend/routes/MentorRoutes.js
+++ bigbyte/Backend/routes/MentorRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const AuthenticationController = require('../controllers/Authentication');
-const MentorController = require('../controllers/Mentor');
+const MentorController = require('../controllers/Mentors');
 
 
 // Mentor routes
