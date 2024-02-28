d3b8031ac4a217509d0060dee0cf2c0ced567561
andresenriquez1 <andresem858@gmail.com>
2024-02-28 13:20:55 -0800

    solid hopefully

--- bigbyte/Backend/fb.js
+++ bigbyte/Backend/fb.js
@@ -1,15 +1,15 @@
 
 const { initializeApp } = require("firebase/app");
-require('dotenv').config({ path: './Secure.env' }) 
+require('dotenv').config({ path: './.env' }) 
 const firebaseConfig = {
-    apiKey: process.env.FBSDKapiKey,
-    authDomain: process.env.FBSDKapiKey,
-    databaseURL: process.env.FBSDKdatabaseURL,
-    projectId: process.env.FBSDKprojectId,
-    storageBucket: process.env.FBSDKstorageBucket,
-    messagingSenderId: process.env.FBSDKmessagingSenderId,
-    appId: process.env.FBSDKappId,
-    measurementId: process.env.FBSDKmeasurementId
+    apiKey: process.env.apiKey,
+    authDomain: process.env.apiKey,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.KappId,
+    measurementId: process.env.measurementId
   };
   const firebaseApp = initializeApp(firebaseConfig);
 
