1696dad8bff119b3a41126ce265614e4e1088d9e
Nicole Nunez-Sainz <nnunezsa@ucla.edu>
2024-03-04 10:55:10 -0800

    Added pages of intern search and resume in navbar

--- bigbyte/src/App.js
+++ bigbyte/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Login from './pages/Login.js';
 import Signup from './pages/Signup.js';
 import Home from './pages/Home.js';
+import InternshipSearch from './pages/InternshipSearch.js';
+import Resume from './pages/Resume.js';
 import BackendTesting from './pages/BackendFunctionality/BackendTesting';
 import UserDetailsForm from './pages/UserDataSignup.js';
 import ReactDOM from "react-dom/client";
@@ -40,6 +42,8 @@ function App() {
 
         <Route path="/UserData" element={<UserDetailsForm />} />
         <Route path="BackendTesting" element={<BackendTesting />} />
+        <Route path="InternshipSearch" element={<InternshipSearch />} />
+        <Route path="Resume" element={<Resume />} />
 
 
 
---
+++ bigbyte/src/pages/Resume.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import '../Login.css';
+import { Navigate } from 'react-router-dom';
+
+export default function Resume(){
+    return (
+              <h1 class="title">Share your resume</h1>
+    );
+}
