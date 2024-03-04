1163e2f61e02d9867cf317f86eb116dfd6ab56ec
Nicole Nunez-Sainz <nnunezsa@ucla.edu>
2024-03-04 11:42:42 -0800

    Added to resume

--- bigbyte/src/pages/Resume.js
+++ bigbyte/src/pages/Resume.js
@@ -5,6 +5,10 @@ import { Navigate } from 'react-router-dom';
 
 export default function Resume(){
     return (
+        <>
               <h1 class="title">Share your resume</h1>
+              <h1 class="h2">Drop your resume and let your peers help you into 
+              creating the ultimate resume that has been ever seen!</h1>
+              </>
     );
 }
