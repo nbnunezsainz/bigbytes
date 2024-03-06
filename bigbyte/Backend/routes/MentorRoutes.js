cb6a21ebc0ae41704da1683e7c01cbf13e27092a
Christian Gonzalez <christiangonzalez@Christians-MacBook-Pro.local>
2024-03-06 00:50:04 -0800

    added verify token to getAllMentors()

--- bigbyte/Backend/routes/MentorRoutes.js
+++ bigbyte/Backend/routes/MentorRoutes.js
@@ -3,11 +3,12 @@ const router = express.Router();
 
 //const AuthenticationController = require('../controllers/Authentication')
 const MentorController = require('../controllers/Mentors');
+const AuthenticationController = require("../controllers/Authentication");
 
 // Mentor routes
 router.route('/AddMentor').post(MentorController.addMentor);
 router.route('/QueryMentors').get(MentorController.queryMentors);
-router.route('/GetAllMentors').get(MentorController.getAllMentors);
+router.route('/GetAllMentors').get(AuthenticationController.verifyToken,MentorController.getAllMentors);
 router.route('/DeleteMentor').delete(MentorController.deleteMentor);
 router.route('/GetMentor').get(MentorController.getMentor);
 router.route('/GenerateInternship').post(MentorController.generateInternship);
