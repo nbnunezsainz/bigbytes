a08584a89db624a0fa64b87d78f0b08c54260670
Nicole Nunez-Sainz <nnunezsa@ucla.edu>
2024-03-11 07:54:57 -0700

    Added feature of Js.Confetti for Create Internship

--- bigbyte/src/AuthenticatedPages/CreateInternships.js
+++ bigbyte/src/AuthenticatedPages/CreateInternships.js
@@ -5,8 +5,10 @@ import { Container, Row, Col, Button, Form } from 'react-bootstrap';
 import AuthNavbar from './AuthenticatedNavBar';
 import auth from "../fb.js";
 import '../Styling/CreateInternship.css';
+import Confetti from 'react-dom-confetti';
 
-//import './CreateInternship.css'; // Make sure to create this CSS file for styles
+
+
 
 const CreateInternship = () => {
   const [internshipData, setInternshipData] = useState({
@@ -21,7 +23,7 @@ const CreateInternship = () => {
     mentorID: '', // Assuming this will come from authenticated user context or similar
 });
     const [loading, setLoading] = useState(true); // State to manage loading status
-  
+    const [showConfetti, setShowConfetti] = useState(false); // Want to add the js.confetti so when someone submits a posting, they are rewarded for it 
     useEffect(() => {
       // Define the asynchronous function inside the useEffect hook
   
@@ -87,6 +89,7 @@ const CreateInternship = () => {
                         <Col sm ={12}>
                             <div className="create-internship-content">
                                 {/* <h2>Create New Internship</h2> */}
+                                {/* Form starts here */}
                                 <Form onSubmit={handleSubmit} className="internship-form">
                                     <Form.Group className="mb-3" controlId="formTitle">
                                         <Form.Label>Title</Form.Label>
@@ -169,6 +172,7 @@ const CreateInternship = () => {
                                             placeholder="Enter referral limit"
                                         />
                                     </Form.Group>
+                                    {/* Form ends here */}
                                     <Button variant="primary" type="submit">
                                         Submit
                                     </Button>
@@ -177,6 +181,8 @@ const CreateInternship = () => {
                         </Col>
                     </Row>
                 </Container>
+                 {/* Once submitted, call active showConfetti */}
+                <Confetti active={showConfetti} />
             </div>
         );
     };
